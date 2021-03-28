2d3560d55c40005bc3904947a61e4f219575779a
XraiseD <egormmnagib@mail.ru>
2021-03-28 19:33:54 +0300

    верстка регистрации

--- web-app/src/Components/authForm/authForm.js
+++ web-app/src/Components/authForm/authForm.js
@@ -3,8 +3,38 @@ import './authForm.scss';
 
 const Auth = () => (
     <div className='Auth'>
-        <div className='auth-wrapper'></div>
+        <div className='auth-wrapper'>
+            <span className="info-requirement">Введите данные для регистрации</span> 
+                <div className="inputs">
+                    
+                    <div className="nick">
+                        <label>Имя пользователя: </label>
+                        <input type="text" className="reg-input" placeholder="nickname" required />
+                    </div>
+                        <br/>
+                        <div className="first-password">
+                            <label>Пароль: </label>
+                            <input type="text" className="reg-input" placeholder="password" required />
+                        </div>
+                        <br/>
+                        <div className="second-password">
+                            <label>Повторите пароль: </label>
+                            <input type="text" className="reg-input" placeholder="password" required />
+                        </div>
+                </div>
+                <br/>
+                <div className="buttons">
+                    <div className="cancel-button">
+                        <button class="in-button">Cancel</button> 
+                    </div>
+                    <div className="back-button">
+                        <button class="in-button">Back</button> 
+                    </div>
+                    <div className="signin-button">
+                        <button class="in-button">Sign-in</button> 
+                    </div> 
+                </div>
+        </div>
     </div>
 );
-
 export default Auth
