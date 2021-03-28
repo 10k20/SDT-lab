de6634756a072c3f6def63cd2f92b8bf47223803
10k20 <10k20.dev@gmail.com>
2021-03-28 19:35:23 +0300

    update time

--- web-app/src/Components/Header/Header.js
+++ web-app/src/Components/Header/Header.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.scss';
 
-const Header = () => (
-    <header>
-        <div className='header-wrapper'>
-            <div className='logo'>
-                <p>Snake game</p>
+function Header() {
+    const [time, setTime] = useState(Date.now());
+    var currentDate = new Date();
+    useEffect(() => {
+      const interval = setInterval(() => setTime(Date.now()), 5000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
+    return (
+        <header>
+            <div className='header-wrapper'>
+                <div className='logo'>
+                    <p>Snake game</p>
+                </div>
+                <div className='info'>
+                    <p className='login'>login</p>
+                    <p className='time'>{currentDate.getHours() + ':' + currentDate.getMinutes()}</p>
+                </div>
             </div>
-            <div className='info'>
-                <p className='login'>login</p>
-                <p className='time'>16:58</p>
-            </div>
-        </div>
-    </header>
-);
+        </header>
+    );
+}
+
+
 
 export default Header
