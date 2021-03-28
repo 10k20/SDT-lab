6191ab0e49541230116729a3c0cd1fbb848410ff
10k20 <10k20.dev@gmail.com>
2021-03-29 00:09:19 +0300

    add FAQ component

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,6 +4,7 @@ import Snake from './Components/Snake/Snake';
 import Auth from './Components/authForm/authForm';
 import FinalTab from './Components/finalTab/finalTab';
 import MainMenu from './Components/mainMenu/mainMenu';
+import FAQ from './Components/FAQ/FAQ';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={MainMenu} />
         <Route exact path='/play' component={Snake} />
         <Route exact path='/auth' component={Auth} />
+        <Route exact path='/faq' component={FAQ} />
         <Route exact path='/final' component={FinalTab} />
       </Switch>
     </div>
---
+++ web-app/src/Components/FAQ/FAQ.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FAQ.scss';
+
+const FAQ = () => (
+    <div className="FAQ">
+        <div className="FAQ-wrapper"></div>
+    </div>
+);
+
+export default FAQ
