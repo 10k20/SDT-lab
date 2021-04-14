0e66d6a73bc99fecfecdca349c679901de4061ca
10k20 <10k20.dev@gmail.com>
2021-04-14 17:48:27 +0300

    delete console logs

--- web-app/src/Components/signUp/signUp.js
+++ web-app/src/Components/signUp/signUp.js
@@ -4,7 +4,6 @@ import Axios from 'axios';
 import './signUp.scss';
 
 const SignUp = (props) => {
-    console.log(props)
     const history = useHistory();
     const [dataStatus, setDataStatus] = useState('')
 
